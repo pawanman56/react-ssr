83c07357c5d1d7905c5d389599d775ebac3e2a14
Pawan Sunuwar <pawansunuwar@Pawan-3.local>
2022-07-06 16:23:52 +0545

    add css

--- components/Posts/index.js
+++ components/Posts/index.js
@@ -2,6 +2,19 @@ import React from 'react';
 
 const Posts = ({ posts }) => (
     <div>
+        <style jsx>
+        {`
+            .post {
+                max-width: 500px;
+                margin: 0 auto 20px auto;
+            }
+
+            .post h2 {
+                margin-bottom: 10px;
+            }
+        `}
+        </style>
+
         {
             posts.map(post => (
                 <div key={post.id}>
